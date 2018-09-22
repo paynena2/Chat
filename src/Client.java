1633f8e3163dba5acd813621e91d7db953cfa705
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-21 22:39:21 -0400

    Added Graphics, not done.

--- src/Client.java
+++ src/Client.java
@@ -3,11 +3,18 @@
  */
 //Graphics imports
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.layout.StackPane;
 
 //Communication imports
 import java.io.IOException;
@@ -29,8 +36,27 @@ public class Client extends Application{
 		
 		//graphics
 		Stage stage = new Stage();
-		Pane connectionMenu = new Pane();
+		StackPane connectionMenu = new StackPane();
 		Scene connectionScene = new Scene(connectionMenu,500,500);
+		VBox menu = new VBox();
+		HBox ipBox = new HBox();
+		HBox portBox = new HBox();
+		Label connectionTitle = new Label("Allen's Chat Login ");
+		Label ip = new Label("IP:");
+		Label port = new Label("Port:");
+		TextField serverAddressInput = new TextField();
+		TextField serverPortInput = new TextField();
+		Button connectionSubmit = new Button();
+		connectionMenu.setPadding(new Insets(100,100,100,100));
+		menu.setSpacing(75);
+		connectionTitle.setFont(Font.font("TIMES_NEW_ROMAN", FontWeight.BOLD, 24));
+		serverAddressInput.setPromptText("Enter the IP for the server: ");
+		serverPortInput.setPromptText("Enter the port for the server: ");
+		menu.getChildren().addAll(connectionTitle, serverAddressInput, serverPortInput);
+		connectionMenu.getChildren().add(menu);
+		menu.setAlignment(Pos.CENTER);
+		
+		
 		stage.setScene(connectionScene);
 		stage.setTitle("Allen's Chat");
 		stage.show();
